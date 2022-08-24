f04871be6b16d17dc119eb6aba6aa4b517cf38d5
Pham-Xuan-Thanh <sie.phamthanh@gmail.com>
2022-08-24 11:03:56 +0000

    Change thresholde to 3.000.000 W

--- scripts/allowancestake.ts
+++ scripts/allowancestake.ts
@@ -2,7 +2,7 @@ import {ethers} from "hardhat";
 import {Staking} from "../types/Staking";
 
 const STAKING_CONTRACT_ADDRESS = process.env.STAKING_CONTRACT_ADDRESS ?? '';
-const STAKE_AMOUNT = ethers.utils.parseEther("1000000")
+const STAKE_AMOUNT = ethers.utils.parseEther("3000000")
 const VALIDATOR_ACCOUNT = process.env.VALIDATOR_ADDRESS ?? '' ;
 
 
